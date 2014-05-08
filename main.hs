62d5647dde8baede9927a65bc665c87e12d181de
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-08 21:36:51 +0000

    abstractization to Symbol, Index, Tensor, Associator, Product

--- main.hs
+++ main.hs
@@ -1,31 +1,50 @@
 module Main where
 
-data Symbol = A | B | C 
+data Symbol = Symbol Char -- A | B | C | D | E | F
      deriving (Show,Eq,Ord)
 
-data Index = I | J | K | L | M | N
+data Index = Index Char -- I | J | K | L | M | N
            deriving (Show,Eq,Ord)
 -- type Scalar = Symbol
 
 data Tensor = Tensor { symbol :: Symbol, indices :: [Index] }
             deriving (Show,Eq,Ord)
 
-data Monomial = Monomial [Tensor]
-              deriving (Show,Eq,Ord)
+-- data Monomial = Monomial [Tensor]
+--               deriving (Show,Eq,Ord)
 
-a = Tensor A [I,J]
+data Associator = Associator { asymbol :: Symbol, aindices :: [Index] } 
+                deriving (Show,Eq,Ord)
 
-b = Tensor B [K,L] 
 
-mon1 = Monomial [a,b]
 
-monomial2Tensor :: Symbol -> Monomial -> Tensor
-monomial2Tensor s (Monomial xs) = Tensor s (concatMap indices xs)
+data Product = Product [Tensor] [Associator]
+               deriving (Show,Eq,Ord)
+   
+
+s = Symbol
+i = Index 
+
+a = Tensor (s 'A') [i 'I',i 'J']
+
+b = Tensor (s 'B') [i 'K',i 'L'] 
+
+delta = Associator (s 'D') [i 'J',i 'K'] 
+
+
+prod1 = Product [a,b] [delta]
+
+product2Tensor :: Symbol -> Product -> Tensor
+product2Tensor s (Product xs ys) = Tensor s resultIdx
+  where primIdx = concatMap indices xs
+        assocIdx = concatMap aindices ys
+      
+        resultIdx = filter (not . (`elem` assocIdx)) primIdx
 
 
 main :: IO ()
 main = do 
   putStrLn "Haha, I am the omnipotent QFT solver!"
 
-  print mon1  
-  print (monomial2Tensor C mon1)
+  print prod1  
+  print (product2Tensor (s 'C') prod1)

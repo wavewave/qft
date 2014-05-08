d925d560b5730ccf0ee8ed5379ab9e6304dec4ec
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-08 22:16:11 +0000

    implement diff

--- main.hs
+++ main.hs
@@ -1,38 +1,78 @@
 module Main where
 
-data Symbol = Symbol Char -- A | B | C | D | E | F
-     deriving (Show,Eq,Ord)
+newtype Symbol = Symbol { unSymbol :: String }
+               deriving (Show,Eq,Ord)
 
-data Index = Index Char -- I | J | K | L | M | N
-           deriving (Show,Eq,Ord)
--- type Scalar = Symbol
+newtype Index = Index { unIndex :: String }
+              deriving (Show,Eq,Ord)
 
 data Tensor = Tensor { symbol :: Symbol, indices :: [Index] }
             deriving (Show,Eq,Ord)
-
--- data Monomial = Monomial [Tensor]
---               deriving (Show,Eq,Ord)
 
 data Associator = Associator { asymbol :: Symbol, aindices :: [Index] } 
                 deriving (Show,Eq,Ord)
 
 
 
-data Product = Product [Tensor] [Associator]
+data Product = Product { tensor :: [Tensor] 
+                       , assoc :: [Associator]  }
                deriving (Show,Eq,Ord)
-   
+
+
+data Exp = ExpSimpl Tensor
+         | ExpProd Product  
 
 s = Symbol
 i = Index 
 
-a = Tensor (s 'A') [i 'I',i 'J']
+a = Tensor (s "X") [i "I"]
 
-b = Tensor (s 'B') [i 'K',i 'L'] 
+b = Tensor (s "Y") [i "J"]
 
-delta = Associator (s 'D') [i 'J',i 'K'] 
+c = Tensor (s "X") [i "K"]
+
+xl = Tensor (s "X") [i "L"]
+
+zero = Associator (s "0") [] 
+
+deltan idx1 idx2 = Associator (s "deltaN") (idx1 ++ idx2)
+
+-- delta = Associator (s "D") [i "J",i "K"] 
+
+diffTensor :: Tensor -> Tensor -> Associator
+diffTensor x y  
+  | symbol x == symbol y && length (indices x) == length (indices y) = deltan (indices x) (indices y)
+  | otherwise = zero   
 
 
-prod1 = Product [a,b] [delta]
+prod1 = Product [a,b,c] [] --  [delta]
+
+diff :: Product -> Tensor -> [Product]
+diff (Product ts as) x = (filter (not . isZero) . map f  .getHoles []) ts
+  where f (p,(q1,q2)) = Product (q1++q2) (diffTensor p x : as) 
+
+
+ -- undefined -- map (Pro  . (\x -> diffTensor x y) ts 
+
+isZero :: Product -> Bool
+isZero = any ((== "0") . unSymbol . asymbol) . assoc   
+
+
+-- | find holes: for example [x,y,z] to [(x,([],[y,z])), (y,([x],[z])), (z,([y,x],[]))] 
+getHoles :: [a] -> [a] -> [(a,([a],[a]))]
+getHoles ys [] = error "no element" 
+getHoles ys (x:[]) = [(x,(ys,[]))]
+getHoles ys (x:xs) = (x,(ys,xs)) : getHoles (ys++[x]) xs
+
+-- next (xs,y:ys) = Just (y, (xs,ys))
+-- next (xs,[]) = Nothing
+
+
+
+
+
+promoteTensor :: Tensor -> Product
+promoteTensor t = Product [t] []
 
 product2Tensor :: Symbol -> Product -> Tensor
 product2Tensor s (Product xs ys) = Tensor s resultIdx
@@ -44,7 +84,8 @@ product2Tensor s (Product xs ys) = Tensor s resultIdx
 
 main :: IO ()
 main = do 
-  putStrLn "Haha, I am the omnipotent QFT solver!"
-
   print prod1  
-  print (product2Tensor (s 'C') prod1)
+  print (product2Tensor (s "C") prod1)
+
+  print (getHoles [] [1,2,3,4,5])
+  mapM_ print (diff prod1 xl)

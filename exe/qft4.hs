c03f5bf8e102625d8790343896cb212992cd0c45
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-19 13:39:10 +0000

    safe array

---
+++ exe/qft4.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+
+import           Data.Proxy
+import           Unsafe.Coerce
+import           Permute
+
+
+main :: IO ()
+main = do 
+  print $ listFromInterval (mkNatInterval :: NatInterval 1 3)
+
+  let i1 = mkNatInterval :: NatInterval 2 9
+      i2 = regularizeInterval i1 
+  print $ listFromInterval i1
+  print $ listFromInterval i2
+
+  
+
+  let ra = mkRegArray (11 `Cons` (12 `Cons` (13 `Cons` (14 `Cons` (15 `Cons` Nil)))))
+
+
+
+  print (ra ^! 7 )
--- lib/Permute.hs
+++ lib/Permute.hs
@@ -4,30 +4,65 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Permute where
 
 -- import Data.Type.Equality
 import GHC.TypeLits
 import Data.Proxy
-
--- data Proxy (a :: Nat) = Proxy 
-
-data TCMP = TLT | TEQ | TGT
-
-data Compare (a :: Nat) (b :: Nat) (r :: TCMP) where
-  LessThan    :: forall a b x . (a+x) ~ b => Proxy a -> Proxy b -> Compare a b TLT
-  Equal       :: forall a b   . a ~ b     => Proxy a -> Proxy b -> Compare a b TEQ
-  GreaterThan :: forall a b x . a ~ (b+x) => Proxy a -> Proxy b -> Compare a b TGT
-
-test :: Compare a b r -> String 
-test (LessThan _ _)     = "LessThan"
-test (Equal _ _)        = "Equal"
-test (GreaterThan _ _)  = "GreaterThan"
+import Data.Array
 
 
-fun :: IO ()
-fun = do 
-  print "hello"
-  print (test (LessThan (Proxy :: Proxy 1) (Proxy :: Proxy 3)))
-  print (test (Equal (Proxy :: Proxy 1) (Proxy :: Proxy 1)))
+data NatInterval s e where 
+  NatInterval :: forall s e n . (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => 
+                 (Proxy n) -> NatInterval s e
+
+start :: forall s e . (KnownNat s) => NatInterval s e -> Integer
+start _ = natVal (Proxy :: Proxy s) 
+
+end :: forall s e . (KnownNat e) => NatInterval s e -> Integer
+end _ = natVal (Proxy :: Proxy e)
+
+type RegInterval = NatInterval 1
+
+-- | 
+mkNatInterval :: forall s e n . (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => 
+                 NatInterval s e
+mkNatInterval = NatInterval (Proxy :: Proxy n)
+
+
+-- | 
+regularizeInterval :: forall s e n . (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => 
+                      NatInterval s e -> NatInterval 1 n
+regularizeInterval (NatInterval n) = mkNatInterval
+
+
+-- | 
+listFromInterval :: forall s e n . 
+                    (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => NatInterval s e -> [Integer]
+listFromInterval (NatInterval n') = let n = natVal n'
+                                    in take (fromInteger n) $ iterate succ (natVal (Proxy :: Proxy s)) 
+
+data Vector n a where
+   Nil :: Vector 0 a
+   Cons :: forall a n. a -> Vector n a -> Vector (n+1) a
+
+listFromVector :: Vector n a -> [a]
+listFromVector Nil = []
+listFromVector (x `Cons` xs) = x : listFromVector xs
+
+data RegArray n a = RArray (RegInterval n) (Array Integer a)
+
+mkRegArray :: (KnownNat n) => Vector n a -> RegArray n a
+mkRegArray = let ix = mkNatInterval
+             in RArray ix . listArray (start ix, end ix) . listFromVector 
+
+
+
+(^!) :: forall a n . (KnownNat n) => RegArray n a -> Integer -> a 
+(RArray _ arr) ^! i = let nn = natVal (Proxy :: Proxy n) 
+                          ii' = i `mod` nn
+                          ii = if ii' == 0 then nn else ii'
+                      in arr ! ii
+

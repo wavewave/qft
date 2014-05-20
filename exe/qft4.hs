bc24f411d88a8413fd7f93f57ac2a07b278c906b
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-20 13:36:59 +0000

    much cleaner solution

--- exe/qft4.hs
+++ exe/qft4.hs
@@ -9,28 +9,35 @@ import           Permute
 
 main :: IO ()
 main = do 
+  {-
   print $ listFromInterval (mkNatInterval :: NatInterval 1 3)
 
   let i1 = mkNatInterval :: NatInterval 2 9
       i2 = regularizeInterval i1 
   print $ listFromInterval i1
   print $ listFromInterval i2
-
-  let tarr = array (1,3) [(1,2),(2,3),(3,1)]
-      rarr = case (mkRevArray tarr :: Either String RevArray) of
+  -}
+ 
+  let tarr :: Array (Within 3) (Within 3)
+      tarr = array (1, 3) [ (1, 2),(2,3),(3,1)]
+      perm = case (mkPermutation tarr :: Either String (Permutation 3)) of
                Left str -> error str
-               Right x -> x 
-     
+               Right x -> x  
+  print (permute perm 1)
+
+  {-     
   (print . backwardArray) rarr
 
   let p = case (mkPermutation rarr :: Either String (Permutation 3)) of
             Left str -> error str
             Right x -> x 
-      intval = mkNatInterval :: RegInterval 3
+      -- intval = mkNatInterval :: RegInterval 3
   
-  print (permuteBackward p (XInInterval (Proxy :: Proxy 1)))
+  print (permuteBackward p (XInInterval (Proxy :: Proxy 3)))
   
 
-  let b = isElemType (Proxy :: Proxy 1) intval :: Bool
+  -- let b = isElemType (Proxy :: Proxy 1) intval :: Bool
 
-  print b
+  -- print b
+
+  -}
--- lib/Permute.hs
+++ lib/Permute.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE IncoherentInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -16,16 +17,20 @@ module Permute where
 
 import GHC.TypeLits
 -- 
+import Control.Monad (guard)
 import Control.Monad.ST (ST, runST)
 import Control.Monad.Trans (lift)
-import Control.Monad.Trans.Either (runEitherT, left)
+import Control.Monad.Trans.Either (runEitherT, left, hoistEither)
 import Data.Array
 import Data.Array.ST 
 import qualified Data.Foldable as F
+import Data.Promotion.Prelude
 import Data.Proxy
+
 --
 import Unsafe.Coerce
 
+{- 
 data NatInterval s e where 
   NatInterval :: forall s e n . (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => 
                  (Proxy n) -> NatInterval s e
@@ -64,17 +69,58 @@ data XInIntervalBox (s :: Nat) (e :: Nat) where
   MkXInIntervalBox :: forall x s e. (KnownNat s, KnownNat e, KnownNat x, s <= x, x <= e) => 
                       XInInterval x s e -> XInIntervalBox s e
 
--- | 
-data RevArray = RevArray { forwardArray :: Array Integer Integer
-                         , backwardArray :: Array Integer Integer }
+-}
 
-mkRevArray :: Array Integer Integer -> Either String RevArray
-mkRevArray arr= runST action
-  where action :: forall s. ST s (Either String RevArray)        
+guardEither :: String -> Bool -> Either String ()
+guardEither str False = Left str
+guardEither str True = Right ()
+
+maybeEither :: String -> Maybe a -> Either String a
+maybeEither str Nothing = Left str
+maybeEither _ (Just x) = Right x
+
+newtype Within (n :: Nat) = MkWithin Integer 
+                          deriving (Show, Eq, Ord, Enum, Ix) 
+
+
+
+mkWithin :: forall (n :: Nat). (KnownNat n) => Integer -> Maybe (Within n)
+mkWithin v = guard (1 <= v && v <= (natVal (Proxy :: Proxy n))) >> return (MkWithin v)
+
+
+
+mkWithinMod :: forall (n :: Nat) . (KnownNat n) => Integer -> Within n
+mkWithinMod v = MkWithin v' 
+  where nn = natVal (Proxy :: Proxy n)
+        v' = let x = v `mod` nn in if x == 0 then nn else x
+
+instance (KnownNat n) => Num (Within n) where
+  (MkWithin a) + (MkWithin b) = mkWithinMod (a+b)
+  (MkWithin a) * (MkWithin b) = mkWithinMod (a*b)
+  abs (MkWithin a) = mkWithinMod (abs a)
+  signum (MkWithin a) = MkWithin 1
+  fromInteger a = mkWithinMod a
+  negate (MkWithin a) = mkWithinMod (negate a)
+
+
+
+is1 :: Within n -> Bool 
+is1 (MkWithin v) = v == 1
+
+             
+-- |
+data Permutation (n :: Nat) = Permutation { forward :: Array (Within n) (Within n)
+                                          , backward :: Array (Within n)  (Within n) }
+                            deriving (Show)
+
+mkPermutation :: forall (n :: Nat) . (KnownNat n) => Array (Within n) (Within n) -> Either String (Permutation n)
+mkPermutation arr= runST action
+  where action :: forall s. ST s (Either String (Permutation n))        
         action =   runEitherT $ do 
                      let (i1,i2) = bounds arr
-                     rarr <- lift (newArray (i1,i2) Nothing :: ST s (STArray s Integer (Maybe Integer)))
-                     rarr' <- lift (newArray_ (i1,i2) :: ST s (STArray s Integer Integer))
+                     hoistEither (guardEither "i1 is not 1" (is1 i1))
+                     rarr <- lift (newArray (i1,i2) Nothing :: ST s (STArray s (Within n) (Maybe (Within n))))
+                     rarr' <- lift (newArray_ (i1,i2) :: ST s (STArray s (Within n) (Within n)))
                      F.forM_ [i1..i2] $ \i -> do
                        let r = arr ! i
                        o <- lift (readArray rarr r)
@@ -83,35 +129,57 @@ mkRevArray arr= runST action
                          Nothing -> lift (writeArray rarr r (Just i))
                      F.forM_ [i1..i2] $ \r -> 
                        maybe (left "not reversible") (\i -> lift (writeArray rarr' r i)) =<< lift (readArray rarr r)
-                     return . RevArray arr =<< lift (freeze rarr')
+                     return . Permutation arr =<< lift (freeze rarr')
 
-
+{- 
 newtype Permutation (n :: Nat) = Permutation { permmap :: RevArray }
 
 mkPermutation :: forall n. (KnownNat n) => RevArray -> Either String (Permutation n)
-mkPermutation revarr = let intval = mkNatInterval :: RegInterval n 
-                           (i1,i2) = bounds (forwardArray revarr)
-                       in if (i1,i2) == (start intval, end intval) 
-                          then Right (Permutation revarr) 
-                          else Left "interval mistmatch"
-
-{- 
--- |
-permuteForward :: forall i n a b.  
-                  Permutation n 
-               -> NumberInIntervalBox 1 n 
-               -> NumberInIntervalBox 1 n
-permuteForward p (MkNumberInIntervalBox (NumberInInterval i)) = 
-    case someNatVal (forwardArray (permmap p) ! (natVal i)) of 
-      Nothing -> error "impossible"
-      Just (SomeNat (prxy :: Proxy j)) -> MkNumberInIntervalBox (NumberInInterval prxy)
+mkPermutation revarr = if (1, natVal (Proxy :: Proxy n)) == bounds (forwardArray revarr)
+                           then Right (Permutation revarr) 
+                           else Left "interval mistmatch"
 -}
 
-type family F (a :: Nat) :: Nat where
-  F 0 = 1
-  F 1 = 2
-  F a = 3
 
+ 
+-- |
+permute :: Permutation n -> Within n -> Within n
+permute p i = forward p ! i
+
+-- | 
+invperm :: Permutation n -> Within n -> Within n
+invperm p i = backward p ! i
+
+
+{-
+ 
+    case someNatVal (forwardArray (permmap p) ! (natVal i)) of 
+      Nothing -> error "impossible"
+      Just (SomeNat (prxy :: Proxy j)) -> let b = boolean (Proxy :: Proxy (F (1 <=? n))) in undefined 
+
+                                          undefined 
+
+class BooleanKindToType a where 
+  boolean :: a -> Bool 
+
+type family F (a :: Bool) :: * where
+  F True = ()
+  F False = ()
+
+instance BooleanKindToType (Proxy ()) where boolean _ = True
+
+instance BooleanKindToType (Proxy True) where
+  boolean _ = True
+
+instance BooleanKindToType (Proxy False) where
+  boolean _ = False
+
+-}
+
+-- MkNumberInIntervalBox (NumberInInterval prxy)
+
+{- 
+ 
 class IsElemType x set where 
   isElemType :: x -> set -> Bool
 
@@ -131,25 +199,4 @@ permuteBackward :: forall j n a b. (KnownNat n) => Permutation n -> XInInterval 
 permuteBackward p (XInInterval j) = backwardArray (permmap p) ! (natVal j)
 
 
-
-
--- | 
-data Color = Red | Black
-
-data RBTree :: * -> Color -> Nat -> * where
-  RBLeaf :: RBTree a Black 0
-  RBNodeR :: a -> RBTree a Black n -> RBTree a Black n -> RBTree a Red n
-  RBNodeB :: a -> RBTree a c n    -> RBTree a c' n   -> RBTree a Black (n+1)
-
-
-
-data Tree :: * -> * where
-  Leaf :: a -> Tree a 
-  Node :: Tree a -> Tree a -> Tree a
-
-
-data TestType :: Tree Nat -> * where
-  TestTyp1 :: TestType (Leaf 1)
-  TestTyp2 :: TestType (Node (Leaf 2) (Leaf 1))
-
- 
+-}

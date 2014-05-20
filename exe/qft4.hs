0d41bdc4da9859d81708e2391cf2dd0c7fc24788
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-20 05:22:52 +0000

    implement safe permutation

--- exe/qft4.hs
+++ exe/qft4.hs
@@ -2,8 +2,9 @@
 
 module Main where
 
+import           Data.Array
 import           Data.Proxy
-import           Unsafe.Coerce
+-- import           Unsafe.Coerce
 import           Permute
 
 
@@ -16,6 +17,24 @@ main = do
   print $ listFromInterval i1
   print $ listFromInterval i2
 
+{-
   let ra = mkRegArray (11 `Cons` (12 `Cons` (13 `Cons` (14 `Cons` (15 `Cons` Nil)))))
       two = NumberInInterval (Proxy :: Proxy 2) (mkNatInterval :: NatInterval 1 5)
   print (ra ^! two )
+-}
+
+
+  let tarr = array (1,3) [(1,2),(2,3),(3,1)]
+      rarr = case (mkRevArray tarr :: Either String RevArray) of
+               Left str -> error str
+               Right x -> x 
+     
+  (print . backwardArray) rarr
+
+  let p = case (mkPermutation rarr :: Either String (Permutation 4)) of
+            Left str -> error str
+            Right x -> x 
+      intval = mkNatInterval :: RegInterval 4
+  
+  print (permuteForward p (NumberInInterval (Proxy :: Proxy 1) intval))
+  
--- lib/Permute.hs
+++ lib/Permute.hs
@@ -8,11 +8,15 @@
 
 module Permute where
 
--- import Data.Type.Equality
 import GHC.TypeLits
-import Data.Proxy
+-- 
+import Control.Monad.ST (ST, runST)
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Either (runEitherT, left)
 import Data.Array
-
+import Data.Array.ST 
+import qualified Data.Foldable as F
+import Data.Proxy
 
 data NatInterval s e where 
   NatInterval :: forall s e n . (KnownNat s, KnownNat e, KnownNat n, (n+s) ~ (e+1)) => 
@@ -43,6 +47,7 @@ listFromInterval :: forall s e . (KnownNat s, KnownNat e) => NatInterval s e -> 
 listFromInterval (NatInterval n') = let n = natVal n'
                                     in take (fromInteger n) $ iterate succ (natVal (Proxy :: Proxy s)) 
 
+{- 
 -- | 
 data Vector n a where
    Nil :: Vector 0 a
@@ -56,23 +61,60 @@ listFromVector (x `Cons` xs) = x : listFromVector xs
 -- |
 data RegArray n a = RArray (RegInterval n) (Array Integer a)
 
-
 -- | 
 mkRegArray :: (KnownNat n) => Vector n a -> RegArray n a
 mkRegArray = let ix = mkNatInterval
              in RArray ix . listArray (start ix, end ix) . listFromVector 
-
--- |
-(^!) :: forall i n a b. (KnownNat n) => RegArray n a -> NumberInInterval i 1 n -> a 
-(RArray _ arr) ^! (NumberInInterval p _ ) 
-    = let nn = natVal (Proxy :: Proxy n) 
-          ii = natVal p
-      in arr ! ii
+-}
 
 
 data NumberInInterval x s e where
   NumberInInterval :: forall x s e . (KnownNat s, KnownNat e, KnownNat x, s <= x , x <= e) =>
                       Proxy x -> NatInterval s e -> NumberInInterval x s e
+
+
+data NumberInIntervalBox s e = forall x. MkNumberInIntervalBox (NumberInInterval x s e) 
+
+-- | 
+data RevArray = RevArray { forwardArray :: Array Integer Integer
+                         , backwardArray :: Array Integer Integer }
+
+mkRevArray :: Array Integer Integer -> Either String RevArray
+mkRevArray arr= runST action
+  where action :: forall s. ST s (Either String RevArray)        
+        action =   runEitherT $ do 
+                     let (i1,i2) = bounds arr
+                     rarr <- lift (newArray (i1,i2) Nothing :: ST s (STArray s Integer (Maybe Integer)))
+                     rarr' <- lift (newArray_ (i1,i2) :: ST s (STArray s Integer Integer))
+                     F.forM_ [i1..i2] $ \i -> do
+                       let r = arr ! i
+                       o <- lift (readArray rarr r)
+                       case o of
+                         Just _ -> left "not reversible"
+                         Nothing -> lift (writeArray rarr r (Just i))
+                     F.forM_ [i1..i2] $ \r -> 
+                       maybe (left "not reversible") (\i -> lift (writeArray rarr' r i)) =<< lift (readArray rarr r)
+                     return . RevArray arr =<< lift (freeze rarr')
+
+
+newtype Permutation (n :: Nat) = Permutation { permmap :: RevArray }
+
+mkPermutation :: forall n. (KnownNat n) => RevArray -> Either String (Permutation n)
+mkPermutation revarr = let intval = mkNatInterval :: RegInterval n 
+                           (i1,i2) = bounds (forwardArray revarr)
+                       in if (i1,i2) == (start intval, end intval) 
+                          then Right (Permutation revarr) 
+                          else Left "interval mistmatch"
+
+-- |
+permuteForward :: forall i n a b. (KnownNat n) => Permutation n -> NumberInInterval i 1 n -> Integer
+permuteForward p (NumberInInterval i _) = forwardArray (permmap p) ! (natVal i)
+
+-- | 
+permuteBackward :: forall j n a b. (KnownNat n) => Permutation n -> NumberInInterval j 1 n -> Integer
+permuteBackward p (NumberInInterval j _) = backwardArray (permmap p) ! (natVal j)
+
+
 
 
 
@@ -82,10 +124,20 @@ data NumberInInterval x s e where
 -- | 
 data Color = Red | Black
 
-data Tree :: * -> Color -> Nat -> * where
-  Leaf :: Tree a Black 0
-  NodeR :: a -> Tree a Black n -> Tree a Black n -> Tree a Red n
-  NodeB :: a -> Tree a c n    -> Tree a c' n   -> Tree a Black (n+1)
+data RBTree :: * -> Color -> Nat -> * where
+  RBLeaf :: RBTree a Black 0
+  RBNodeR :: a -> RBTree a Black n -> RBTree a Black n -> RBTree a Red n
+  RBNodeB :: a -> RBTree a c n    -> RBTree a c' n   -> RBTree a Black (n+1)
 
 
-  
+
+data Tree :: * -> * where
+  Leaf :: a -> Tree a 
+  Node :: Tree a -> Tree a -> Tree a
+
+
+data TestType :: Tree Nat -> * where
+  TestTyp1 :: TestType (Leaf 1)
+  TestTyp2 :: TestType (Node (Leaf 2) (Leaf 1))
+
+ 

8d2f4c87ac55df58621920460de8269985c64596
Ian-Woo Kim <ianwookim@gmail.com>
2014-06-02 14:39:00 +0000

    code clean up

--- lib/Data/Enumerated/SeqZipper.hs
+++ lib/Data/Enumerated/SeqZipper.hs
@@ -10,35 +10,25 @@ module Data.Enumerated.SeqZipper where
 
 import GHC.TypeLits
 --
--- import Control.Applicative hiding (empty)
--- import Data.Foldable
--- import Data.Monoid ((<>))
--- import Data.Sequence 
 import           Data.Singletons
--- import Data.Traversable
 import Data.Type.Equality
 --
 import           Data.Enumerated.Sequence hiding (singleton)
--- import qualified Data.EnumeratedSequence as N (singleton)
 import           Data.PeanoNat
 -- 
 
--- import Prelude hiding (zipWith, length, splitAt)
-
-
 -- |
 data NSeqZipper' (m :: PNat) (n :: PNat) a where
-  --        current   lefts        rights
-  NSZ :: a -> NSeq' m a -> NSeq' n a -> NSeqZipper' m n a
+  --     current    lefts        rights
+  NSZ ::       a -> NSeq' m a -> NSeq' n a -> NSeqZipper' m n a
 
+-- | to use type-level literal natural number
 type NSeqZipper (m :: Nat) (n :: Nat) = NSeqZipper' (FromNat m) (FromNat n) 
 
 -- |
 singleton :: a -> NSeqZipper 0 0 a  
 singleton x = NSZ x empty empty
 
- 
- 
 -- | 
 toFirst :: forall m n a . (SingI m, SingI n) => 
          NSeqZipper' m n a -> NSeqZipper' PZero (m :+: n) a 
@@ -73,7 +63,6 @@ toRight (NSZ x ls rs) = let rview = viewl (sing :: Sing (PSucc n)) rs
                         in case rview of 
                              y :< ys -> NSZ y (ls |> x) ys
 
-
 ------------------------
 -- fetch focused item -- 
 ------------------------
@@ -89,7 +78,6 @@ prev = current . toLeft
 -- |
 next :: (SingI m, SingI n) => NSeqZipper' m (PSucc n) a -> a
 next = current . toRight
-
 
 ------------------------------
 -- operation on focues item --
@@ -107,10 +95,6 @@ replace x = modify (const x)
 -- |
 delete :: forall m n m' n' a. (SingI m, SingI n, SingI m', SingI n') => NSeqZipper' m n a -> NSeqZipper' m' n' a
 delete (NSZ _ ls rs) = 
-  case sing
-
-
-
   case viewl ys of 
     EmptyL -> case viewr xs of 
                 EmptyR -> Nothing 
@@ -118,63 +102,7 @@ delete (NSZ _ ls rs) =
     z :< zs -> Just (SZ (z,(xs,zs)))
 -}
 
-
 -- toSeq :: SeqZipper a -> Seq a
 -- toSeq (SZ (x,(x1s,x2s))) = x1s >< (x <| x2s)
 
 
-{- 
--- |
-deriving instance Functor SeqZipper
-
--- |
-deriving instance Foldable SeqZipper 
-
--- |
-instance Applicative SeqZipper where
-  pure = singletonSZ 
-  SZ (f,(f1s,f2s)) <*> SZ (x,(y1s,y2s)) = SZ (f x, (zipWith id f1s y1s, zipWith id f2s y2s))
-
--- |
-deriving instance Traversable SeqZipper 
-
--- |
-lengthSZ :: SeqZipper n a -> Int 
-lengthSZ (SZ (_x, (x1s,x2s))) = length x1s + length x2s + 1 
-
--- |
-currIndex :: SeqZipper a -> Int
-currIndex (SZ (_x, (x1s,_x2s))) = length x1s 
-
-
--- |
-appendGoLast :: SeqZipper a -> a -> SeqZipper a
-appendGoLast (SZ (y,(y1s,y2s))) x = SZ (x, ((y1s |> y) >< y2s, empty))
-
--- |
-chopFirst :: SeqZipper a -> Maybe (SeqZipper a)
-chopFirst (SZ (y,(y1s,y2s))) = 
-  case viewl y1s of
-    EmptyL -> case viewl y2s of 
-                EmptyL -> Nothing 
-                z :< zs -> Just (SZ (z,(empty,zs)))
-    _z :< zs -> Just (SZ (y,(zs,y2s)))
-    
-
--- |
-moveTo :: Int -> SeqZipper a -> Maybe (SeqZipper a) 
-moveTo n orig@(SZ (x,(x1s,x2s))) = 
-  let n_x1s = length x1s 
-      n_x2s = length x2s 
-      res | n < 0 || n > n_x1s + n_x2s = Nothing 
-          | n == n_x1s = Just orig 
-          | n < n_x1s = let (x1s1, x1s2) = splitAt n x1s 
-                            el :< rm = viewl x1s2
-                        in Just (SZ (el, (x1s1,(rm |> x) >< x2s)))
-          | n > n_x1s = let (x2s1,x2s2) = splitAt (n-n_x1s-1) x2s
-                            el :< rm = viewl x2s2
-                        in Just (SZ (el, ((x1s |> x) >< x2s1, rm)))
-          | otherwise = error "error in moveTo"
-  in res 
-
--}
--- lib/Data/Enumerated/Sequence.hs
+++ lib/Data/Enumerated/Sequence.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
@@ -17,42 +16,21 @@ module Data.Enumerated.Sequence where
 
 import GHC.TypeLits
 --
-import Control.Applicative
-import Control.Monad (MonadPlus)
+import           Control.Applicative
+import           Control.Monad (MonadPlus)
 import qualified Data.Foldable as F
-import Data.Monoid (Monoid)
+import           Data.Monoid (Monoid)
 import           Data.Singletons
-import Data.Traversable
+import           Data.Traversable
 import qualified Data.Sequence as S
--- import           Data.Proxy
--- import Data.Typeable (Typeable)
--- import Data.Type.Equality
 --
 import Data.PeanoNat
 
-
+-- | 
 newtype NSeq' (n :: PNat) a = NSeq (S.Seq a)
 
+-- | to use type-level literal natural number
 type NSeq (n :: Nat) = NSeq' (FromNat n) 
-
-
-{- 
-data NSeq (n :: Nat) a where
-  NSeq :: Proxy n -> S.Seq a -> NSeq n a
-
-instance Functor (NSeq n) where
-  fmap f (NSeq prxy s) = NSeq prxy (fmap f s)        
-
-instance F.Foldable (NSeq n) where
-  foldr f z (NSeq _ s) = F.foldr f z s
-
-instance Applicative (NSeq n) where
-  pure x = NSeq (Proxy :: Proxy n) (pure x)
-  (NSeq _ f) <*> (NSeq prxy x) = NSeq  prxy (f <*> x)
-
-instance Traversable (NSeq n) where
-  sequenceA (NSeq prxy s) = NSeq prxy <$> sequenceA s
--}
 
 deriving instance Functor (NSeq' n)
 
@@ -70,11 +48,7 @@ deriving instance MonadPlus (NSeq' n)
 
 deriving instance Monoid (NSeq' n a)
 
--- deriving instance Typeable (NSeq n)
-
 deriving instance (Eq a) => Eq (NSeq' n a) 
-
--- deriving instance (Data a) => Data (NSeq n a)
 
 deriving instance (Ord a) => Ord (NSeq' n a)
 
@@ -82,31 +56,35 @@ deriving instance (Read a) => Read (NSeq' n a)
 
 deriving instance (Show a) => Show (NSeq' n a)
 
-
-
+-- |
 empty :: NSeq 0 a
 empty = NSeq S.empty
 
+-- |
 singleton :: a -> NSeq 1 a
 singleton x = NSeq (S.singleton x)
 
+-- | left append
 (<|) :: a -> NSeq' n a -> NSeq' (PSucc n) a
 x <| NSeq xs = NSeq (x S.<| xs)
-
-infixr 5 ><
 infixr 5 <|
-infixl 5 |>
 
+-- | right append
 (|>) :: forall n a. NSeq' n a -> a -> NSeq' (PSucc n) a
 NSeq xs |> x = NSeq (xs S.|> x)
+infixl 5 |>
 
+-- | concat
 (><) :: forall m n a. NSeq' m a -> NSeq' n a -> NSeq' (m :+: n) a
 NSeq xs >< NSeq ys = NSeq (xs S.>< ys)
+infixr 5 ><
 
+-- | left viewer
 data ViewL' (n :: PNat) a where
   EmptyL :: ViewL' PZero a
   (:<) :: a -> NSeq' n a -> ViewL' (PSucc n) a
  
+-- | left view function
 viewl :: Sing n -> NSeq' n a -> ViewL' n a
 viewl PSZero _  = EmptyL
 viewl (PSSucc _) (NSeq s) =  
@@ -114,29 +92,15 @@ viewl (PSSucc _) (NSeq s) =
       S.EmptyL -> error "viewl: impossible" -- totality guaranteed. 
       x S.:< xs -> x :< NSeq xs
 
+-- | right viewer
 data ViewR' (n :: PNat) a where
   EmptyR :: ViewR' PZero a
   (:>) :: NSeq' n a -> a -> ViewR' (PSucc n) a
 
+-- | right view function
 viewr :: Sing n -> NSeq' n a -> ViewR' n a
 viewr PSZero _ = EmptyR
 viewr (PSSucc _) (NSeq s) = 
     case S.viewr s of
       S.EmptyR -> error "viewr: impossible" -- totality guaranteed
       xs S.:> x -> NSeq xs :> x
-
-
-{- 
-data ViewL (n :: Nat) a where
-  EmptyL :: ViewL 0 a
-  (:<) :: a -> NSeq n a -> ViewL (n+1) a
- 
-viewl :: forall a n. NSeq n a -> ViewL n a
-viewl (NSeq proxy s) = 
-  case testEquality (Proxy :: Proxy 0) proxy of 
-    Just _ -> EmptyL 
-    Nothing -> undefined -- case S.viewl s of
-                         -- x S.:< xs -> x :< (NSeq Proxy xs)
--}
-
-   
